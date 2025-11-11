92ac9e264e93372d84bf5d6e54fe6f83564bd4c7
Oscar Valdivieso <fernandoscar04@gmail.com>
2025-11-10 23:16:03 -0600

    Clean template

--- Admin/src/app/pages/pages-routing.module.ts
+++ Admin/src/app/pages/pages-routing.module.ts
@@ -8,45 +8,6 @@ const routes: Routes = [
       import('./dashboards/dashboards.module').then((m) => m.DashboardsModule),
   },
   {
-    path: 'ui',
-    loadChildren: () => import('./ui/ui.module').then((m) => m.UiModule),
-  },
-  {
-    path: 'advance-ui',
-    loadChildren: () =>
-      import('./advanceui/advanceui.module').then((m) => m.AdvanceuiModule),
-  },
-  {
-    path: 'icons',
-    loadChildren: () =>
-      import('./icons/icons.module').then((m) => m.IconsModule),
-  },
-  {
-    path: 'charts',
-    loadChildren: () =>
-      import('./charts/charts.module').then((m) => m.ChartsModule),
-  },
-  {
-    path: 'tables',
-    loadChildren: () =>
-      import('./table/table.module').then((m) => m.TableModule),
-  },
-  {
-    path: 'forms',
-    loadChildren: () =>
-      import('./forms/forms.module').then((m) => m.FormModule),
-  },
-  {
-    path: 'custom-ui',
-    loadChildren: () =>
-      import('./custom-ui/custom-ui.module').then((m) => m.CustomUiModule),
-  },
-  {
-    path: 'pages',
-    loadChildren: () =>
-      import('./extrapages/extrapages.module').then((m) => m.ExtrapagesModule),
-  },
-  {
     path: 'uni',
     loadChildren: () =>
       import('./UNI/UNI.module').then((m) => m.UNIModule),
